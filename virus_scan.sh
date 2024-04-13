63cc968127a1dd91a569cdfe1ad55450ce3536b6
Trevor Maliro <trevor.t.maliro@gmail.com>
2024-04-13 13:00:35 -0400

    yes

--- virus_scan.sh
+++ virus_scan.sh
@@ -11,3 +11,12 @@ sudo clamscan -r / --bell --infected --remove --recursive --log=/var/log/clamav/
 sudo chkrootkit
 sudo rkhunter --check --skip-keypress --report-warnings-only
 sudo rkhunter --update --propupd
+
+#Enable the mirror checks.
+#UPDATE_MIRRORS=1
+
+#Tells rkhunter to use any mirror.
+#MIRRORS_MODE=0
+
+#Specify a command which rkhunter will use when downloading files from the Internet
+#WEB_CMD=""
